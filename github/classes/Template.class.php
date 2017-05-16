5cc9f0d27d5480bde208414e8e604df5bbb23fa4
tipo94 <tipo94@users.noreply.github.com>
2017-05-16 12:58:48 +0200

    Update Template.class.php

--- github/classes/Template.class.php
+++ github/classes/Template.class.php
@@ -2,9 +2,16 @@
 class Template{
 
 
-	function loadView($template){
+	function loadView($template, array $variables=[]){
 
+			$wwwUrl= __DIR__."/../views/";
+			$requestUrl= __DIR__."/../controllers/";
+			if(count($variables)>0){
 
+				foreach ($variables as $key => $value) {
+					$$key=$value;
+				}
+			}
 			include __DIR__."/../views/LayoutView.phtml";
 	}
 
