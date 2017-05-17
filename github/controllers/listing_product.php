c13a077983243c3fe4d10bb08cff1f2e51b552f4
tipo94 <tipo94@users.noreply.github.com>
2017-05-17 09:38:03 +0200

    Update listing_product.php

--- github/controllers/listing_product.php
+++ github/controllers/listing_product.php
@@ -1,8 +1,15 @@
 <?php
 class Listing_product{
     public function httpGetMethod(){
+
+    	$teaModel= new TeaModel();
+    	$teas=$teaModel->listAll();
+
+    	$categoryModel = new CategoryModel();
+    	$categories= $categoryModel->listAll();
+
         $view= new Template();
-        $view->loadView('listing_product');
+        $view->loadView('listing_product', ['teas'=>$teas, 'categories'=>$categories]);
     }
     public function httpPostMethod(){
         $view= new Template();
