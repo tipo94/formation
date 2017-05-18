711f1cbe404cd22df10254229372674849914216
tipo94 <tipo94@users.noreply.github.com>
2017-05-18 10:29:24 +0200

    Update product.php

--- github/controllers/product.php
+++ github/controllers/product.php
@@ -6,10 +6,18 @@ class Product{
 
     public function httpGetMethod(){
 
-
+    	if(array_key_exists('id', $_GET)){
+    		$id=$_GET['id'];
+    	}
+    	else
+    	{
+    		$id=1;
+    	}
         $view= new Template();
+        $teaModel= new TeaModel();
+  		$tea=$teaModel->listone($id);
 
-        $view->loadView('product');
+        $view->loadView('product', ['tea'=>$tea]);
     }
 
     public function httpPostMethod(){
